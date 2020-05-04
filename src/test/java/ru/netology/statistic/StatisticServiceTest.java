06474ab4492325fe1141659e2ffad335ab6f548d
Valeriia <lera-bezborodova@mail.ru>
2020-05-04 16:15:16 +0300

    Initial commit

---
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticServiceTest {
+
+    @Test
+    void findMax() {StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {8, 5, 12, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+
+}
